de8799a86317b5e32ddbc23331f9664b20ed0570
ggpp200 <ggpp20000@gmail.com>
2023-06-02 23:49:52 +0900

    0602 router 사용취소

--- src/app/search/[searchTerm]/loding.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-function Loading({}: Props) {
-  return <div>Loading.............</div>
-}
-
-export default Loading
--- src/app/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type props = {}
-
-function SearchHome({}: props) {
-  return <div>Plese Search whatever rou want to Search</div>
-}
-
-export default SearchHome
